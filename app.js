2b1ecda87e630bb8cf8fb2853de2820f2f14055d
Elvin <elvinmonteiro77@gmail.com>
2023-02-26 18:08:54 -0500

    add conditional for when value is divisible by 3

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let maxVal = parseInt(prompt('Please enter the number you would like to FizzBuzz up to: '));
 
 for (let i = 1; i <= maxVal; i++) {
-    console.log(i);
+    if (i % 3 === 0) {
+        console.log('Fizz');
+    } else console.log(i);
+
+
 }
