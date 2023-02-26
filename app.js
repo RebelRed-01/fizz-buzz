946276d6ab5e951500db8f13f6a056fb43de9ba0
Elvin <elvinmonteiro77@gmail.com>
2023-02-26 18:32:18 -0500

    display values in the window

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let maxVal = parseInt(prompt('Please enter the number you would like to FizzBuzz up to: '));
+let numbers = document.getElementById('numbers');
+
 
 for (let i = 1; i <= maxVal; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log('Fizz Buzz');
+        numbers.innerText += ' Fizz Buzz,';
     } else if (i % 3 === 0) {
-        console.log('Fizz');
+        numbers.innerText += ' Fizz,';
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+        numbers.innerText += ' Buzz,';
     } else {
-        console.log(i);
+        numbers.innerText += ` ${i},`;
     }
 
 
