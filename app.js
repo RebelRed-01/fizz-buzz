f4b48a09a42dae27671a7cb224056477f05a9ddb
Elvin <elvinmonteiro77@gmail.com>
2023-02-26 18:22:05 -0500

    conditional for when value is divisible by 3 & 5

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let maxVal = parseInt(prompt('Please enter the number you would like to FizzBuzz up to: '));
 
 for (let i = 1; i <= maxVal; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizz Buzz');
+    } else if (i % 3 === 0) {
         console.log('Fizz');
     } else if (i % 5 === 0) {
-        console.log('Buzz')
+        console.log('Buzz');
     } else {
         console.log(i);
     }
